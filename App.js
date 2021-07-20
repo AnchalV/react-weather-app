bf45c25b79b8d8b8d4fd69dbcef2e8adb04ed991
anchal <anchalvirdi46@gmail.com>
2021-07-20 23:20:46 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./index.css";
+import TempApp from "./TempApp"
+
+
+const App = ()=>
+{
+  return (
+    <>
+        <TempApp />
+    </>
+  )
+}
+
+
+export default App;
